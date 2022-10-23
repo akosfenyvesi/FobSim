e56ca25ffa5f5330c87b0edeff88614741746832
Fenya96 <fenya96@gmail.com>
2022-10-23 12:30:13 +0200

    add flask to FobSim

---
+++ web_implementation/run.py
@@ -0,0 +1,41 @@
+from flask import Flask, redirect, url_for, render_template, request, send_file, flash
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/simulation", methods=["POST", "GET"])
+def simulation():
+    if request.method == "POST":
+        data = request.form
+        return redirect(url_for('results'))
+    return render_template("settings.html")
+
+
+@app.route("/results")
+def results():
+    return render_template("results.html")
+
+
+@app.route("/previous-simulations")
+def previous_simulations():
+    return render_template("previoussettings.html")
+
+
+@app.route("/documentation")
+def docs():
+    return render_template("documentation.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ web_implementation/static/js/script.js
@@ -0,0 +1,142 @@
+import json_data from '../data/Sim_parameters.json' assert {type: 'json'};
+
+let blockchainFunction, blockchainPlacement, consensusAlgorithm;
+let numFogNodes, numUsers, numTasks, numMiners, numNeighbours, numTxBlock, puzzleDiff, poetBlockTime, maxPayment,
+    intialWalletValue, miningAward, fogNodeDelay, endUserDelay, assymKeyLength, dposDelegates, gossipActivated,
+    automaticAuthorization, parrallelMining;
+
+$(document).ready(function () {
+    initForm();
+    fillForm();
+
+    let current_fs, next_fs, previous_fs;
+    let opacity;
+
+    $(".next").click(function () {
+        disableInputs();
+
+        current_fs = $(this).parent();
+        next_fs = $(this).parent().next();
+
+        next_fs.show();
+
+        current_fs.animate({opacity: 0}, {
+            step: function (now) {
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                next_fs.css({'opacity': opacity});
+            }, duration: 600
+        });
+    });
+
+    $(".previous").click(function () {
+        current_fs = $(this).parent();
+        previous_fs = $(this).parent().prev();
+
+        previous_fs.show();
+
+        $(":input").prop("disabled", false);
+
+        current_fs.animate({opacity: 0}, {
+            step: function (now) {
+                opacity = 1 - now;
+
+                current_fs.css({
+                    'display': 'none',
+                    'position': 'relative'
+                });
+                previous_fs.css({'opacity': opacity});
+            }, duration: 600
+        });
+    });
+
+    $(".start").click(function () {
+        console.log("Clicked start");
+        // TODO: start simulation
+    });
+});
+
+function initForm() {
+    blockchainFunction = $('#selectFunction');
+    blockchainPlacement = $('#selectPlacement');
+    consensusAlgorithm = $('#selectConsensus');
+
+    numFogNodes = $('#NumOfFogNodes');
+    numUsers = $('#num_of_users_per_fog_node');
+    numTasks = $('#NumOfTaskPerUser');
+    numMiners = $('#NumOfMiners');
+    numNeighbours = $('#number_of_each_miner_neighbours');
+    numTxBlock = $('#numOfTXperBlock');
+    puzzleDiff = $('#puzzle_difficulty');
+    poetBlockTime = $('#poet_block_time');
+    maxPayment = $('#Max_enduser_payment');
+    intialWalletValue = $('#miners_initial_wallet_value');
+    miningAward = $('#mining_award');
+    fogNodeDelay = $('#delay_between_fog_nodes');
+    endUserDelay = $('#delay_between_end_users');
+    assymKeyLength = $('#Asymmetric_key_length');
+    dposDelegates = $('#Num_of_DPoS_delegates');
+    gossipActivated = $('#Gossip_Activated');
+    automaticAuthorization = $('#Automatic_PoA_miners_authorization');
+    parrallelMining = $('#Parallel_PoW_mining');
+}
+
+function fillForm() {
+    numFogNodes.val(json_data['NumOfFogNodes'] >= 1 ? json_data['NumOfFogNodes'] : 1);
+    numUsers.val(json_data['num_of_users_per_fog_node'] >= 1 ? json_data['num_of_users_per_fog_node'] : 1);
+    numTasks.val(json_data['NumOfTaskPerUser'] >= 1 ? json_data['NumOfTaskPerUser'] : 1);
+    let numOfMinersMax = blockchainPlacement.val() === "1" ? numFogNodes.val() : 1500;
+    numMiners.val(2 <= json_data['NumOfMiners'] && json_data['NumOfMiners'] <= numOfMinersMax ? json_data['NumOfMiners'] : 2);
+    numNeighbours.val(1 <= json_data['number_of_each_miner_neighbours'] && json_data['number_of_each_miner_neighbours'] < numMiners.val() ? json_data['number_of_each_miner_neighbours'] : 1);
+    numTxBlock.val(json_data['numOfTXperBlock'] >= 1 ? json_data['numOfTXperBlock'] : 1);
+    puzzleDiff.val(1 <= json_data['puzzle_difficulty'] && json_data['puzzle_difficulty'] <= 64 ? json_data['puzzle_difficulty'] : 1);
+    poetBlockTime.val(json_data['poet_block_time'] >= 1 ? json_data['poet_block_time'] : 1);
+    maxPayment.val(json_data['Max_enduser_payment'] >= 1 ? json_data['Max_enduser_payment'] : 1);
+    intialWalletValue.val(json_data['miners_initial_wallet_value'] >= 0 ? json_data['NumOfTaskPerUser'] : 0);
+    miningAward.val(json_data['mining_award'] >= 0 ? json_data['mining_award'] : 0);
+    fogNodeDelay.val(json_data['delay_between_fog_nodes'] >= 0 ? json_data['delay_between_fog_nodes'] : 0);
+    endUserDelay.val(json_data['delay_between_end_users'] >= 0 ? json_data['delay_between_end_users'] : 0);
+    assymKeyLength.val(json_data['Asymmetric_key_length'] >= 1 ? json_data['Asymmetric_key_length'] : 1);
+    dposDelegates.val(1 <= json_data['Num_of_DPoS_delegates'] && json_data['Num_of_DPoS_delegates'] <= numMiners.val() ? json_data['Num_of_DPoS_delegates'] : 1);
+    gossipActivated.prop('checked', json_data['Gossip_Activated']);
+    automaticAuthorization.prop('checked', json_data['Automatic_PoA_miners_authorization?']);
+    parrallelMining.prop('checked', json_data['Parallel_PoW_mining']);
+}
+
+function disableInputs() {
+    switch (blockchainPlacement.val()) {
+        case "1":
+
+            break;
+        case "2":
+
+            break;
+    }
+
+    switch (consensusAlgorithm.val()) {
+        case "1":
+            poetBlockTime.prop('disabled', true);
+            dposDelegates.prop('disabled', true);
+            automaticAuthorization.prop('disabled', true);
+            break;
+        case "2":
+
+            break;
+        case "3":
+
+            break
+        case "4":
+
+            break;
+        case "5":
+
+            break
+        case "6":
+
+            break;
+    }
+}
